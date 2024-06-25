0b1260c5efe2ec5da957f1264edebce1eccf8b90
joepasss@gentoo <joepasss@gmail.com>
2024-06-26 04:52:22 +0900

    react init

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@styles/index.scss";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <h3>HELLO!</h3>
+  </React.StrictMode>
+);
